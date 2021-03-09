cbf02fbe09ab45be2df5da31d187067a167c8e6e
HadrienVerrier <verrier.hadrien.pro@gmail.com>
2021-03-09 17:36:34 +0100

    continue player design

--- Js/player.js
+++ Js/player.js
@@ -1,11 +1,21 @@
-//CSS
-
 $(document).ready(function () {
+	//CSS
+
+	//OPEN PLAYER AND CLOSE
 	$("#player-open").on("click", function () {
 		if ($("#player-container").hasClass("close")) {
 			$("#player-container").removeClass("close").addClass("open");
 		} else {
 			$("#player-container").removeClass("open").addClass("close");
+		}
+	});
+
+	//SEARCH BAR
+	$("#searchbar").on("input", function () {
+		if (!$(this).val()) {
+			$("#results").addClass("empty");
+		} else {
+			$("#results").removeClass("empty");
 		}
 	});
 });

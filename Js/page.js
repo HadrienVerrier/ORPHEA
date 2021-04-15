a64a18bafa8d35944e53af89f1fb4856daa45ee5
HadrienVerrier <verrier.hadrien.pro@gmail.com>
2021-04-15 20:57:56 +0200

    add play pause function in loopPreview

--- Js/page.js
+++ Js/page.js
@@ -865,8 +865,8 @@ $("body").on("click", ".popup .loop-edit", function () {
 //PREVIEW LOOP IN PLAYER
 
 $("body").on("click", ".popup #loop_results article .loop-play", function () {
-	// console.log($(this).parent().attr("id").split("-")[1]);
 	getLoop($(this).parent().attr("id").split("-")[1]);
+	loopLoad = true;
 });
 ///////////////
 ////FUNCTION///
